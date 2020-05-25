321004fa6f51d4812fe1ddaa15a598422a99b30f
Gino <neodracarys@gmail.com>
2020-05-25 18:19:16 +0800

    Initial commit

---
+++ src/main/java/com/thenogicode/testbenchspring/TestbenchspringApplication.java
@@ -0,0 +1,13 @@
+package com.thenogicode.testbenchspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbenchspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbenchspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thenogicode/testbenchspring/TestbenchspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thenogicode.testbenchspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbenchspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
